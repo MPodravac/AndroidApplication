a79029ff65f2f967bc57f408d8290d2204f590d0
Mateja Podravac <mateja.podravac@gmail.com>
2018-11-10 20:03:34 +0100

    LV2

--- app/src/main/java/com/example/matejapodravac/myapplication/PersonalInfoActivity.java
+++ app/src/main/java/com/example/matejapodravac/myapplication/PersonalInfoActivity.java
@@ -13,22 +13,32 @@ public class PersonalInfoActivity extends AppCompatActivity {
     private Button oBtnUnesi;
     private EditText oInputIme;
     private String sIme;
+    private EditText oInputPrezime;
+    private String sPrezime;
+    private EditText oInputDatumRodenja;
+    private String sDatumRodenja;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
 
-        oInputIme=(EditText)findViewById(R.id.editIme);
-        oBtnUnesi=(Button)findViewById(R.id.button3);
+        oInputIme = (EditText)findViewById(R.id.editIme);
+        oInputPrezime = (EditText)findViewById(R.id.editPrezime);
+        oInputDatumRodenja = (EditText)findViewById(R.id.editDatumRodenja);
+        oBtnUnesi = (Button)findViewById(R.id.button3);
 
         oBtnUnesi.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 sIme = oInputIme.getText().toString();
-                Intent oUpisiIme = new Intent(getApplicationContext(), StudentInfoActivity.class);
-                oUpisiIme.putExtra("ime", sIme);
-                startActivity(oUpisiIme);
+                sPrezime = oInputPrezime.getText().toString();
+                sDatumRodenja = oInputDatumRodenja.getText().toString();
+                Intent oUpisiOsnovnePodatke = new Intent(getApplicationContext(), StudentInfoActivity.class);
+                oUpisiOsnovnePodatke.putExtra("ime", sIme);
+                oUpisiOsnovnePodatke.putExtra("prezime", sPrezime);
+                oUpisiOsnovnePodatke.putExtra("datum rodenja", sDatumRodenja);
+                startActivity(oUpisiOsnovnePodatke);
             }
 
         });
--- app/src/main/java/com/example/matejapodravac/myapplication/SummaryActivity.java
+++ app/src/main/java/com/example/matejapodravac/myapplication/SummaryActivity.java
@@ -9,10 +9,22 @@ import android.widget.TextView;
 
 public class SummaryActivity extends AppCompatActivity {
 
-    private String ime;
+    private String sIme;
+    private String sPrezime;
+    private String sDatumRodenja;
     private String sPredmet;
+    private String sProfesor;
+    private String sAkGodina;
+    private String nPredavanja;
+    private String nVjezbe;
     private TextView oIme;
+    private TextView oPrezime;
+    private TextView oDatumRodenja;
     private TextView oPredmet;
+    private TextView oProfesor;
+    private TextView oAkGodina;
+    private TextView oPredavanja;
+    private TextView oVjezbe;
     private Button oBtnPotvrdi;
 
     @Override
@@ -21,19 +33,37 @@ public class SummaryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_summary);
 
         final Bundle oExtras=getIntent().getExtras();
-        ime=oExtras.getString("ime");
-        sPredmet=oExtras.getString("predmet");
+        sIme = oExtras.getString("ime");
+        sPrezime = oExtras.getString("prezime");
+        sDatumRodenja = oExtras.getString("datum rodenja");
+        sPredmet = oExtras.getString("predmet");
+        sProfesor = oExtras.getString("profesor");
+        sAkGodina = oExtras.getString("ak godina");
+        nPredavanja = oExtras.getString("predavanja");
+        nVjezbe = oExtras.getString("vjezbe");
 
-        oIme=(TextView)findViewById(R.id.textViewIme);
-        oIme.setText(ime);
-        oPredmet=(TextView)findViewById(R.id.textViewPredmet);
+        oIme = (TextView)findViewById(R.id.textViewIme);
+        oIme.setText(sIme);
+        oPrezime = (TextView)findViewById(R.id.textViewPrezime);
+        oPrezime.setText(sPrezime);
+        oDatumRodenja = (TextView)findViewById(R.id.textViewDatumRodenja);
+        oDatumRodenja.setText(sDatumRodenja);
+        oPredmet = (TextView)findViewById(R.id.textViewPredmet);
         oPredmet.setText(sPredmet);
+        oProfesor = (TextView)findViewById(R.id.textViewProfesor);
+        oProfesor.setText(sProfesor);
+        oAkGodina = (TextView)findViewById(R.id.textViewAkGodina);
+        oAkGodina.setText(sAkGodina);
+        oPredavanja = (TextView)findViewById(R.id.textViewPredavanja);
+        oPredavanja.setText(nPredavanja);
+        oVjezbe = (TextView)findViewById(R.id.textViewVjezbe);
+        oVjezbe.setText(nVjezbe);
 
         oBtnPotvrdi = (Button)findViewById(R.id.btnPotvrdi);
         oBtnPotvrdi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent oPotvrdi=new Intent(getApplicationContext(), PersonalInfoActivity.class);
+                Intent oPotvrdi = new Intent(getApplicationContext(), PersonalInfoActivity.class);
                 startActivity(oPotvrdi);
             }
         });
