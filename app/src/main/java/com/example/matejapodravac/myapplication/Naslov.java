83e2e44205b3f8453356e3989dbaaf0eebf75ee5
Mateja Podravac <mateja.podravac@gmail.com>
2018-11-22 20:09:53 +0100

    strings

---
+++ app/src/main/java/com/example/matejapodravac/myapplication/Naslov.java
@@ -0,0 +1,11 @@
+package com.example.matejapodravac.myapplication;
+
+public class Naslov
+{
+    public String sNaslov;
+    public Naslov(String sNaslov)
+    {
+        this.sNaslov = sNaslov;
+    }
+
+}
